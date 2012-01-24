4c8062648a21ae76371283cd033d9b278458ccbf
student2 <student2@eficode-academy-2.(none)>
2012-01-24 16:09:25 +0200

    library

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = 'c5d759ddca47a732d082ed6b54609f5bf1b0aafa8f1d8ed90bdb03a599eddf6beb5313ec4397f960ecf18b363f7c73143444474c3fc827762cd6974fde6b9997'
